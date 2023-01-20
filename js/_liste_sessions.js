9a1524427c676d2d365f18cb50e34268b0c0d990
Tate Mastin <thmastin@gmail.com>
2023-01-19 21:17:54 -0500

    Initial Commit

---
+++ js/_liste_sessions.js
@@ -0,0 +1,5 @@
+liste_sessions = [];
+last_sessionid = 59078185;
+last_sessionnum = 0;
+liste_sessions = liste_sessions.concat([{'sessionid': 59078185, 'sessionnum': 0, 'track': 'Daytona International Speedway - Road Course', 'car': 'Ferrari 488 GT3 Evo 2020', 'annee': 2023, 'mois': 1, 'jour': 18, 'type': 'Practice', 'nom': 'Practice, 2:00:00 - 2023-01-18 17h15 UTC - Daytona International Speedway - Road Course - Ferrari 488 GT3 Evo 2020'}]);
+liste_sessions = liste_sessions.concat([{'sessionid': 59067143, 'sessionnum': 0, 'track': 'Daytona International Speedway - Road Course', 'car': 'Ferrari 488 GT3 Evo 2020', 'annee': 2023, 'mois': 1, 'jour': 18, 'type': 'Practice', 'nom': 'Practice, 4:00:00 - 2023-01-18 01h00 UTC - Daytona International Speedway - Road Course - Ferrari 488 GT3 Evo 2020'}]);
